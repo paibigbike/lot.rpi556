053d60896a0b32d8e1b8f6b075a4cbae00ec0fc4
Pai <chatponc65@nu.ac.th>
2022-12-14 11:44:05 +0700

    Move the status button into its owm class with the toggle function moving to a method.

--- src/app/comm_mqtt.py
+++ src/app/comm_mqtt.py
@@ -6,37 +6,49 @@ sensor data, using the paho mqtt library
 """
 
 __author__ = "Chatpon Chaimongkol"
+
 # standand library
 import time
 
 # installled library
 import paho.mqtt.client as mqtt
 
-HIVEMQTT_PORT = 1883 # CONSTANT
+HIVEMQTT_PORT = 1883  # CONSTANT
 HIVEMQTT_BROKER = "broker.hivemq.com"
-PUBLISH_TOPIC= "Naresuan/Chatpon"
-SUBSCRIBE_TOPIC= "Naresuan/+"
-
-def on_subscription(*args):
-    print("subscribed:", args)
-
-def on_connection(*args):
-    """ Call back for when mqtt connect to the broker
-    and prints out on acknowleadgement and subsceibes"""
-    print("Connected")
-    client.subscribe(SUBSCRIBE_TOPIC)
-
-def on_massage(client, user_data, msg:mqtt.MQTTMessage):
-    print("got massage", msg.payload)
+PUBLISH_TOPIC = "Naresuan/Chatpon"
+SUBSCRIBE_TOPIC = "Naresuan/+"
 
 
-client = mqtt.Client()
-client.on_connect = on_connection
-client.on_subscribe = on_subscription
-client.on_message = on_massage
-client.connect(HIVEMQTT_BROKER, HIVEMQTT_PORT)
-client.loop_start()
-while True:
-    client.publish(PUBLISH_TOPIC,
-                   "hello_this is Chatpon ")
-    time.sleep(5)
+class MQTTConn:
+    def __init__(self):
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connection
+        # self.client.on_subscribe = on_subscription
+        self.client.on_message = self.on_massage
+        self.client.connect(HIVEMQTT_BROKER, HIVEMQTT_PORT)
+        self.client.loop_start()
+
+    def publish(self, topic, massage):
+        self.client.publish(topic, massage)
+
+    #def on_subscription(*args):
+        #print("subscribed:", args)
+
+    def on_connection(self, *args):
+        """ Call back for when mqtt connect to the broker
+        and prints out on acknowleadgement and subsceibes"""
+        print("Connected")
+        self.client.subscribe(SUBSCRIBE_TOPIC)
+
+    @staticmethod
+    def on_massage(client, user_data, msg: mqtt.MQTTMessage):
+        print("got massage", msg.payload)
+
+
+if __name__ == '__main__':
+    client = MQTTConn()
+
+    while True:
+        client.publish(PUBLISH_TOPIC,
+                       "hello_this is Chatpon ")
+        time.sleep(5)
--- src/app/main_qui2.py
+++ src/app/main_qui2.py
@@ -8,7 +8,32 @@ from a remote sensor through a mqtt communication channel
 __author__ = "Chatpon Chaimongkol"
 
 # Libraries / modules / packages
-import tkinter
+import tkinter as tk
+
+# local files
+import comm_mqtt
+
+
+class SensorUI(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)  # initialize parent class
+        self.comm = comm_mqtt.MQTTConn()
+        status_frame = tk.Frame(self, relief=tk.RIDGE, borderwidth=5)
+        self.status_buttons = []
+        print("1: ", self.status_buttons)
+        for i in range(4):
+            print("i = ", i)
+            status_btn = StatusButton(status_frame)
+
+        status_frame.pack(side=tk.TOP)
+        # make a button (tkinter class) and put it in the app
+        # Button take 2 arguments, app where to put the button
+        # text - key word argument
+
+        tk.Button(self, text="Change status",
+                  command=status_btn.toggle_color).pack(side=tk.TOP)
+
 
 class StatusButton:
     """ Display the status using a canvas
@@ -17,13 +42,14 @@ class StatusButton:
                 canvas (Tk.Canvas) : canvas the circle in
                 color (str) : color the circle will show
     """
+
     def __init__(self, parent):
 
         self.color = 'red'
-        self.canvas = tkinter.Canvas(parent, width=120, height=120)
+        self.canvas = tk.Canvas(parent, width=120, height=120)
         self.circle = self.canvas.create_oval(10, 10, 110, 110,
                                               fill=self.color)
-        self.canvas.pack()
+        self.canvas.pack(side=tk.LEFT)
 
     def toggle_color(self):
         """ Toggle color between red and green """
@@ -36,30 +62,15 @@ class StatusButton:
 
         self.canvas.itemconfig(self.circle, fill=self.color)
 
+
 if __name__ == '__main__':
-    app = tkinter.Tk()  # appercation a class of tkinter.Tk
+    app = SensorUI()  # appercation a class of tkinter.Tk
+    app2 = SensorUI()
     # geometry is a method of the tkinter.Tk class that
     # sets the size of the app window . It takes a
     # string as an argument.
 
     app.geometry("400x400")
-    satus_btn = StatusButton(app)
-    print(satus_btn)
-    satus_btn2 = StatusButton(app)
-    print(satus_btn2)
-
-    # make a button (tkinter class) and put it in the app
-    # Button take 2 arguments, app where to put the button
-    # text - key word argument
-
-    tkinter.Button(app, text="Toggle Circle 1",
-                   command=satus_btn.toggle_color).pack()
-    tkinter.Button(app, text="Toggle Circle 1",
-                   command=satus_btn2.toggle_color).pack()
-
+    app2.geometry("400x400")
     app.mainloop()  # mainloop is method of tkinter.Tk
     # method
-    print('====')
-    print(satus_btn.toggle_color)
-    print(satus_btn.toggle_color())
-
