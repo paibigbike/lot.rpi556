52c3a17b120f37f5878c184c528c92ddec2b2374
Pai <chatponc65@nu.ac.th>
2023-01-13 22:40:21 +0700

    Midterm part 2 Build on your Rock paper Scissors game

---
+++ src/app/midterm_chatpon/pai2.py
@@ -0,0 +1,168 @@
+# Copyright (C) 2022 Chatpon Chaimongkol <chatponc65@nu.ac.th>
+
+"""
+
+Make a GUI to play Rock Paper Scissor over the internet
+
+"""
+
+__author__ = "Chatpon Chaimongkol"
+
+# standard libra
+import tkinter as tk
+import pai_comm  # MQTT
+import random
+
+class Rock_Paper_Scissors(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.comm = pai_comm.MQTTConn(self)  # MQTT
+
+        # Rock
+        self.rock_button = tk.Button(self, text="Rock",
+                                     font=20, command=self.rock)
+        self.rock_button.pack()
+
+        # Scissors
+        self.scissors_button = tk.Button(self, text="Scissors",
+                                         font=20, command=self.scissors)
+        self.scissors_button.pack()
+
+        # Paper
+        self.paper_button = tk.Button(self, text="Paper",
+                                      font=20, command=self.paper)
+        self.paper_button.pack()
+
+        self.player_choice = " "
+
+        self.result_user = tk.Label(self, text="")
+        self.result_user.pack()
+
+        self.result_AI = tk.Label(self, text="")
+        self.result_AI.pack()
+
+        self.result = tk.Label(self, text="")
+        self.result.pack()
+
+    def rock(self):
+        """
+        Toggle the stat of the sensor True -> False or False -> True,
+        update the text of the run button, update the local StatusButton,
+        and send the proper mqtt message
+
+        """
+        self.player_choice = 'Rock'
+        self.result_user.config(text="You picked Rock", font=20)
+
+        msg1 = "Player pick Rock"  # MQTT
+        self.comm.publish(msg1)    # MQTT
+        computer = random.choice(["rock", "paper", "scissors"])
+
+        if computer == "rock":
+            self.result_AI.config(text="Com picked Rock", font=20)
+            msg2 = "Com pick Rock"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="Tie!", font=42)
+            msg3 = "Tie"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+        elif computer == "paper":
+            self.result_AI.config(text="Com picked Paper", font=20)
+            msg2 = "Com pick Paper"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="You lose!", font=42)
+            msg3 = "You lose!"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+        elif computer == "scissors":
+            self.result_AI.config(text="Com picked Scissors", font=20)
+            msg2 = "Com pick Scissors"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="You win!", font=42)
+            msg3 = "You win!"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+    def scissors(self):
+        self.player_choice = 'Scissors'
+        self.result_user.config(text="You picked Scissors", font=20)
+
+        msg1 = "Scissors"  # MQTT
+        self.comm.publish(msg1)  # MQTT
+        computer = random.choice(["rock", "paper", "scissors"])
+
+        if computer == "rock":
+            self.result_AI.config(text="Com picked Rock", font=20)
+            msg2 = "Com pick Rock"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="You lose!", font=42)
+            msg3 = "You lose!"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+        elif computer == "paper":
+            self.result_AI.config(text="Com picked Paper", font=20)
+            msg2 = "Com pick Paper"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="You win!", font=42)
+            msg3 = "You win!"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+        elif computer == "scissors":
+            self.result_AI.config(text="Com picked Scissors", font=20)
+            msg2 = "Com pick Scissors"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="Tie!", font=42)
+            msg3 = "Tie"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+    def paper(self):
+        self.player_choice = 'Paper'
+        self.result_user.config(text="You picked Paper", font=20)
+
+        msg1 = "Paper"  # MQTT
+        self.comm.publish(msg1)  # MQTT
+        computer = random.choice(["rock", "paper", "scissors"])
+
+        if computer == "rock":
+            self.result_AI.config(text="Com picked Rock", font=20)
+            msg2 = "Com pick Rock"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="You win!", font=42)
+            msg3 = "You win!"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+        elif computer == "paper":
+            self.result_AI.config(text="Com picked Paper", font=20)
+            msg2 = "Com pick Paper"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="Tie!", font=42)
+            msg3 = "Tie"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+        elif computer == "scissors":
+            self.result_AI.config(text="Com picked Scissors", font=20)
+            msg2 = "Com pick Scissors"  # MQTT
+            self.comm.publish(msg2)  # MQTT
+
+            self.result.config(text="You lose!", font=42)
+            msg3 = "You lose!"  # MQTT
+            self.comm.publish(msg3)  # MQTT
+
+
+if __name__ == "__main__":
+    app = Rock_Paper_Scissors()   # application a class ot tkinter.Tk
+    # geometry is a method of the tkinter.Tk class that
+    # sets the size of the app window. It takes a
+    # string as on argument
+    app.geometry("400x400")
+    app.title('Rock Paper Scissors game')
+    app.mainloop()  # mainloop is method of tkinter.Tk
+    # methods are function of cla
---
+++ src/app/midterm_chatpon/pai_comm.py
@@ -0,0 +1,81 @@
+# Copyright (C) 2022 Chatpon Chaimongkol <chatponc65@nu.ac.th>
+
+"""
+communicate with the HIVEMQ broker to receive
+sensor data,using the paho matt library
+"""
+
+__author__ = "Chatpon Chaimongkol"
+
+import paho.mqtt.client as mqtt
+import pai2
+import random
+import time
+
+# installed library
+import paho.mqtt.client as mqtt
+
+HIVEMQTT_PORT = 1883  # CONSTANT
+HIVEMQTT_BROKER = "broker.hivemq.com"
+PUBLIST_TOPIC = "Naresuan/Chatpon"
+SUBSCRIBE_TOPIC = "Naresuan/+"
+msg1 = ""
+msg2 = ""
+msg3 = ""
+
+
+class MQTTConn:
+    """
+    Use the paho library to connect to the HIVE MQ mqtt broker
+
+    Attributes
+        root(main_gui.SensorUI): root user interface app
+        client (mqtt.Client): paho client for mqtt communication
+    """
+    def __init__(self, root: pai2.Rock_Paper_Scissors):
+        self.root = root
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connection
+        #self.client.on_subscribe = self.on_subscription
+        self.client.on_message = self.on_message
+        self.client.connect(HIVEMQTT_BROKER, HIVEMQTT_PORT)
+        self.client.loop_start()
+
+    def publish(self, message):
+        """
+        Send a message to the HIVE MQ broker using the PUBLISH_TOPIC
+        Args:
+            message(str): massage to send
+
+        Returns:
+
+        """
+        self.client.publish(PUBLIST_TOPIC, message)
+
+    def on_connection(self, *args):
+        """call back for when mqtt connects to the broken
+        and prints out acknowledgment and subscribes"""
+        print("Connected")
+        self.client.subscribe(SUBSCRIBE_TOPIC)
+
+    def on_message(self, client, user_data, msg: mqtt.MQTTMessage):
+        """
+        Callback when receiving message
+        Args:
+            client:
+            user_data:
+            msg(mqtt.MQTTMessage): message received
+
+        """
+
+        print("got message: ", msg.payload)
+        print("from topic", msg.topic)
+        name = msg .topic .split('/')[-1]
+        print("message from: ", name)
+
+
+
+if __name__ == '__main__':
+    test_client = MQTTConn(None)
+
+
